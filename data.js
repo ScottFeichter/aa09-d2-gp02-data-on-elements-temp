9671ef54e8f1a6766317ab2e91f8ad23889cc4d4
Scott Feichter <scottfeichter@gmail.com>
2024-04-17 10:25:39 -0700

    yes! yes, yes

--- data.js
+++ data.js
@@ -1,1 +1,29 @@
-// Your code here 
+// Your code here
+
+const createLi = (e) => {
+  e.preventDefault();
+  const nameInput = document.getElementById(`name`);
+  const typeInput = document.getElementById(`type`);
+  const shoppingList = document.getElementById(`shopping-list`);
+
+  const name = nameInput.value;
+  const type = typeInput.value;
+
+  const li = document.createElement("li");
+  li.innerText = name;
+  li.dataset.type = type;
+
+  nameInput.value = ``;
+  typeInput.value = `dairy`;
+
+  console.log(li);
+  shoppingList.appendChild(li);
+}
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  const addButton = document.getElementById(`add`);
+
+
+  addButton.addEventListener(`click`, createLi);
+
+});
